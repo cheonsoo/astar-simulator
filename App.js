cf253e4bfc4e311ae900de42f026ff3bca205232
chance <cs.park@gsshop.com>
2022-05-05 00:02:44 +0900

    ios build

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { WebView } from 'react-native-webview';
 import AstarSimulator from './components/astarSimulator';
 
 export default function App() {
@@ -15,9 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#eee',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 50
   },
+  webview: {}
 });
